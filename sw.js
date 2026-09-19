570b5a23ada905a94bf1f5439d42cb6b43e11c6e
MRjoxa2101 <murodovjavoxir774@gmail.com>
2026-09-19 21:30:15 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = "maqsadlarim-v1";
+const FILES = ["./", "./maqsadlarim_sayti.html", "./manifest.json", "./icon.svg"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => cached)));
+});
